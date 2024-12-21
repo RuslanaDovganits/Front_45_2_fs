3abf29564d01f827963c6bbd4ab2da009fa95f81
Ruslana Dovganits <ruslanadovganits@yahoo.com>
2024-12-21 08:51:27 +0200

    homework

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@
 
 // импорт css
 import './App.css'
-import Lesson01 from './lessons/lesson01/Lesson01'
+import Homework01 from './homeworks/homework01/homework01'
+// import Lesson01 from './lessons/lesson01/Lesson01'
+// import Lesson02 from './lessons/lesson02/Lesson02'
 
 function App() {
   
 
   return (
     <>
-     <Lesson01/>
+     {/* <Lesson01/> */}
+     {/* <Lesson02/> */}
+     <Homework01/>
      
     </>
   )
---
+++ src/components/myButtons/MyButton.jsx
@@ -0,0 +1,6 @@
+// пример отдельного компонента кнопки 
+// экспорт по умолчанию перед обьявлением функции
+
+export default function MyButton() {
+return <button>Click me!</button>
+}
