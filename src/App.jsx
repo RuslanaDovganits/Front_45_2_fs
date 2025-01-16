ad1c9566d036901758a3c48c08b244734ea57071
Ruslana Dovganits <ruslanadovganits@yahoo.com>
2025-01-16 18:43:49 +0200

    homework

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,12 @@
 // импорт css
 import "./App.css";
 // import FellowshipHeroes from "./components/fellowship/FellowshipHeroes";
-import Homeework05 from "./homeworks/homework05/Homework05";
 
-// import Homeework05 from "./homeworks/homework05/Homework05";
-// import Feedback from './components/feedback/Feedback'
-// import Homework01 from "./homeworks/homework01/Homework01";
-// import Homework02 from './homeworks/homework02/Homework02';
-// import Homework03 from './homeworks/homework03/Homework03';
-// import Homework04 from "./homeworks/homework04/Homework04";
-
+import Homework01 from "./homeworks/homework01/Homework01";
+import Homework02 from "./homeworks/homework02/Homework02";
+import Homework03 from "./homeworks/homework03/Homework03";
+import Homework04 from "./homeworks/homework04/Homework04";
+import Homework05 from "./homeworks/homework05/Homework05";
 // import Lesson01 from './lessons/lesson01/Lesson01'
 // import Lesson02 from './lessons/lesson02/Lesson02'
 // import Lesson03 from './lessons/lesson03/Lesson03'
@@ -26,11 +23,11 @@ function App() {
       {/* <Lesson03/> */}
       {/* <Lesson04/> */}
       {/* <Lesson05 /> */}
-      {/* <Homework01 /> */}
-      {/* <Homework02 /> */}
-      {/* <Homework03 /> */}
-      {/* <Homework04 /> */}
-    <Homeework05/>
+      <Homework01 />
+      <Homework02 />
+      <Homework03 />
+      <Homework04 />
+      <Homework05/>
     </>
   );
 }
